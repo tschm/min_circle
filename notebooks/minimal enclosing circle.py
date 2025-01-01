a7977c3f781763540dad92f9d521f533c0e2bcdf
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-01 14:48:40 +0400

    using hexaly (#22)

--- notebooks/minimal enclosing circle.py
+++ notebooks/minimal enclosing circle.py
@@ -1,7 +1,21 @@
 import marimo
 
-__generated_with = "0.10.8"
+__generated_with = "0.10.9"
 app = marimo.App(width="medium")
+
+
+@app.cell
+def _(mo):
+    mo.md("""# Problem""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        """We compute the radius and center of the smallest enclosing ball for $N$ points in $d$ dimensions. We use a variety of tools and compare their performance."""
+    )
+    return
 
 
 @app.cell
@@ -13,7 +27,7 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md("""# Implement with cvxpy""")
+    mo.md(r"""## Generate a cloud of points""")
     return
 
 
@@ -23,12 +37,6 @@ def _():
     import numpy as np
 
     return go, np
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""# Generate a cloud of points""")
-    return
 
 
 @app.cell
@@ -58,44 +66,31 @@ def _(go, pos):
 
     # Show the plot
     fig.show()
+
+    # plot makes really only sense when using d=2
     return (fig,)
 
 
 @app.cell
-def _():
-    import cvxpy as cp
-
-    def min_circle_cvx(points, **kwargs):
-        # cvxpy variable for the radius
-        r = cp.Variable(1, name="Radius")
-        # cvxpy variable for the midpoint
-        x = cp.Variable(points.shape[1], name="Midpoint")
-
-        objective = cp.Minimize(r)
-        constraints = [cp.norm(point - x) <= r for point in points]
-
-        problem = cp.Problem(objective=objective, constraints=constraints)
-        problem.solve(**kwargs)
-
-        return {"Radius": r.value, "Midpoint": x.value}
-
-    return cp, min_circle_cvx
+def _(mo):
+    mo.md("""## Compute with cvxpy""")
+    return
 
 
 @app.cell
-def _(cp, np):
-    def min_circle_cvx_cone(points, **kwargs):
-        # cvxpy variable for the radius
-        r = cp.Variable(1, name="Radius")
-        # cvxpy variable for the midpoint
-        x = cp.Variable(points.shape[1], name="Midpoint")
+def _(np):
+    import cvxpy as cp
 
-        objective = cp.Minimize(r)
-        # slow, but faster than [cp.norm(point - x) <= r for point in points]
-        # constraints = [cp.SOC(r, point - x) for point in points]
+    # We compare 3 equivalent ways to create the constraint that
+    # each point has to be within the ball of radius r centered at x
+    def con_1(points, x, r):
+        return [cp.norm(point - x) <= r for point in points]
 
-        # fast, but somewhat clumsy syntax
-        constraints = [
+    def con_2(points, x, r):
+        return [cp.SOC(r, point - x) for point in points]
+
+    def con_3(points, x, r):
+        return [
             cp.SOC(
                 r * np.ones(points.shape[0]),
                 points - cp.outer(np.ones(points.shape[0]), x),
@@ -103,12 +98,29 @@ def _(cp, np):
             )
         ]
 
+    def min_circle_cvx(points, fct=None, **kwargs):
+        # Use con_1 if no constraint construction is defined
+        fct = fct or con_1
+        # cvxpy variable for the radius
+        r = cp.Variable(1, name="Radius")
+        # cvxpy variable for the midpoint
+        x = cp.Variable(points.shape[1], name="Midpoint")
+
+        objective = cp.Minimize(r)
+        constraints = fct(points, x, r)
+
         problem = cp.Problem(objective=objective, constraints=constraints)
         problem.solve(**kwargs)
 
         return {"Radius": r.value, "Midpoint": x.value}
 
-    return (min_circle_cvx_cone,)
+    return con_1, con_2, con_3, cp, min_circle_cvx
+
+
+@app.cell
+def _(con_3, min_circle_cvx, pos):
+    min_circle_cvx(points=pos, fct=con_3, solver="MOSEK")
+    return
 
 
 @app.cell
@@ -118,8 +130,8 @@ def _(min_circle_cvx, pos):
 
 
 @app.cell
-def _(min_circle_cvx_cone, pos):
-    min_circle_cvx_cone(points=pos, solver="MOSEK")
+def _(mo):
+    mo.md("""## Compute with Mosek""")
     return
 
 
@@ -154,6 +166,12 @@ def _(min_circle_mosek, pos):
 
 
 @app.cell
+def _(mo):
+    mo.md("""## Compute with Hexaly""")
+    return
+
+
+@app.cell
 def _(np):
     import hexaly.optimizer
 
@@ -179,7 +197,7 @@ def _(np):
             model.close()
 
             optimizer.solve()
-            return {"Radius": r.value, "Midpoint": z.level}
+            return {"Radius": r.value, "Midpoint": z[0].value}
 
     return hexaly, min_circle_hexaly
 
@@ -191,19 +209,13 @@ def _(min_circle_hexaly, pos):
 
 
 @app.cell
-def _():
+def _(mo):
+    mo.md("""## Compute with scikit-learn""")
     return
 
 
 @app.cell
-def _(min_circle_cvx, pos):
-    min_circle_cvx(points=pos, solver="MOSEK")
-    return
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md("""# Implement with Mosek Fusion""")
+def _():
     return
 
 
