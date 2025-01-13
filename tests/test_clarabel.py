c7e712c1be95e4112bebf63c1ef22d5640be67d7
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-13 19:19:58 +0400

    testing clarabel

--- tests/test_clarabel.py
+++ tests/test_clarabel.py
@@ -4,26 +4,20 @@ import cvxpy as cp
 import pytest
 
 
-def con_3(points, x, r):
-    return [
-        cp.SOC(
-            r * np.ones(points.shape[0]),
-            points - cp.outer(np.ones(points.shape[0]), x),
-            axis=1,
-        )
-    ]
-
-
 def min_circle_cvx(points, **kwargs):
-    # Use con_1 if no constraint construction is defined
-    fct = con_3
     # cvxpy variable for the radius
     r = cp.Variable(1, name="Radius")
     # cvxpy variable for the midpoint
     x = cp.Variable(points.shape[1], name="Midpoint")
 
     objective = cp.Minimize(r)
-    constraints = fct(points, x, r)
+    constraints = [
+        cp.SOC(
+            r * np.ones(points.shape[0]),
+            points - cp.outer(np.ones(points.shape[0]), x),
+            axis=1,
+        )
+    ]
 
     problem = cp.Problem(objective=objective, constraints=constraints)
     problem.solve(**kwargs)
